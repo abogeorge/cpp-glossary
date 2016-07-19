d560d6d543540d9b4e32398c8cf7ec6adc5388f9
Abordioaie <abordioaie.george@yahoo.com>
2016-07-19 12:11:56 +0300

    Added typedefs

--- CPPGlossary/include/const_macro.h
+++ CPPGlossary/include/const_macro.h
@@ -18,6 +18,11 @@ which leads to an increased exe size.*/
 data type. They are like a preistoric generic programming form*/
 #define sumMac(a,b) a+b
 
+/* A namespace is designed to overcome this difficulty and is used 
+as additional information to differentiate similar functions, classes, 
+variables etc. with the same name available in different libraries. 
+Using namespace, you can define the context in which names are defined. 
+In essence, a namespace defines a scope.  */
 namespace const_macro
 {
   inline void constPrinting()
--- CPPGlossary/include/typedefs.h
+++ CPPGlossary/include/typedefs.h
@@ -3,11 +3,32 @@
 
 #include <iostream>
 
+/*The typedef-names are aliases for existing types, and are not declarations of new types. 
+Typedef cannot be used to change the meaning of an existing type name (including a typedef-name). 
+Once declared, a typedef-name may only be redeclared to refer to the same type again. 
+Typedef names are only in effect in the scope where they are visible: different functions 
+or class declarations may define identically-named types with different meaning.*/
+
 namespace typedefs
 {
+
+  typedef int intreg;
+  
+  typedef struct { int a; int b; } s;
+
+  typedef int int_t, *intp_t, arr_t[10];
+
   inline void typedefPrinting()
   {
-    
+    intreg numI = 3;
+    std::cout << "Printing alias for int: " << numI << std::endl;
+    numI += 4;
+    std::cout << "Printing alias for int + 4: " << numI << std::endl;
+    s testS;
+    testS.a = 3;
+    testS.b = 4;
+    std::cout << "Struct with alias: " << testS.a << ", " << testS.b << std::endl;
+    std::cout << "Sum with struct elements: " << testS.a + testS.b << std::endl;
   }
 }
 
